eef29870b1c705800dfe55cf1375295a22f8842e
fujineyuki <yuki.fujine.sys@gmail.com>
2017-11-26 14:11:37 +0900

    pointx,poinyの削除
    わかりにくくあまり使っていなかったので削除しました。

--- Assets/C#/ballobjectScript.cs
+++ Assets/C#/ballobjectScript.cs
@@ -5,35 +5,32 @@ public class ballobjectScript : MonoBehaviour {
     public GameObject explosion2;
     public ballScript BallScript;
     private int point = 100;
-    private int points = 0;
-    private int pointx = 0;
     private int Score;
     private int addScore;
+    public int highscore;
     public GameObject highscoreGUI;
     public GameObject scoreGUI;
     public GameObject countGUI;
     public GameObject addScoreGUI;
-    private int e = 0;
     public void counta(int count)
     {
         if (count % 5 == 0 && count != 0)
         {
             Score = Score + ((count / 5) * 10000);
-            pointx += Score;
+            highscore += Score;
             scoreGUI.SendMessage("AddPoint", Score);
         }
     }
     public void haiten(int remove_cnt)
     {
         Score = ((point + (25 * remove_cnt)) * remove_cnt);//ポイントを計算している
-        points = points + Score;//イベント発生のための計算
-        pointx = pointx + Score;
+        highscore += Score;
         addScore = Score;
         scoreGUI.SendMessage("AddPoint", Score);//ポイントの計算をしたものを画面に出しているもの
         addScoreGUI.SendMessage("AddPoint1", addScore);
         if (remove_cnt >= 8 )
         {
-            e += 1;
+
             GameObject[] ball = GameObject.FindGameObjectsWithTag("Respawn");
             foreach (GameObject obs in ball)
             {
@@ -45,23 +42,18 @@ public class ballobjectScript : MonoBehaviour {
             
             if (remove_cnt > 9)
             {
-                pointx = pointx + 50000 + 5000 * (remove_cnt % 10);
+                highscore = highscore + 50000 + 5000 * (remove_cnt % 10);
                 scoreGUI.SendMessage("AddPoint", (50000 + 5000 * (remove_cnt % 10)));
                 addScoreGUI.SendMessage("AddPoint1", (addScore + (50000 + 5000 * (remove_cnt % 10))));
             }
            
             scoreGUI.SendMessage("AddPoint", 50000);
             addScoreGUI.SendMessage("AddPoint1", (addScore + 50000));
-            pointx = pointx + 50000;
+            highscore = highscore + 50000;
             BallScript.SendMessage("DropBall", 60 - remove_cnt);
-            points = 0;
         }
     }
-    public void highScorecount()
-    {
-        int highscore = pointx;
-        highscoreGUI.SendMessage("AddPoint", highscore);
-    }
+    public void highScorecount() { highscoreGUI.SendMessage("AddPoint", highscore); }
     public void ChangeColor(GameObject obj, float transparency)
     {
         SpriteRenderer ballTexture = obj.GetComponent<SpriteRenderer>();
